ee226dd13be748cf8d6c21b6c7ccd984e8f2b3a3
beming-dev <mingfordev@gmail.com>
2021-04-27 17:56:05 +0900

    Refactor code

--- shopping-mall/src/components/About/body.jsx
+++ shopping-mall/src/components/About/body.jsx
@@ -3,10 +3,10 @@ import React from "react";
 class About extends React.Component {
   render() {
     return (
-      <div class="body">
-        <div class="about">
-          <div class="contact">Contact: pinyway@gmail.com</div>
-          <div class="phone">Phone number: 02-XXXX-XXXX</div>
+      <div className="body">
+        <div className="about">
+          <div className="contact">Contact: pinyway@gmail.com</div>
+          <div className="phone">Phone number: 02-XXXX-XXXX</div>
         </div>
       </div>
     );
