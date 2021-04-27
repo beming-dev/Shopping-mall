4b92dc603c51a814e03a9e21b64d9f056f3c24b4
beming-dev <mingfordev@gmail.com>
2021-04-27 14:14:21 +0900

    Update pay page

--- shopping-mall/src/domain/Pay/index.jsx
+++ shopping-mall/src/domain/Pay/index.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import "./style.css"
 
+import Postcode from './Postcode'
+
 export default function Pay(props){
 
     const[product, setProduct] = useState({});
@@ -21,6 +23,13 @@ export default function Pay(props){
         })
     }, []);
 
+    function onClick(){
+        let daum = document.querySelector(".daum");
+
+        daum.classList.toggle('invisible');
+    }
+    
+
     return (
     <div className="pay">
         <div className="productInfo">
@@ -28,7 +37,23 @@ export default function Pay(props){
             <div className="name">{product.name}</div>
             <div className="price">{product.price}won</div>
         </div>
-        <form action=""></form>
+        <form action="" className="payInfo">
+            <label htmlFor="buyer_name">name</label>
+            <input type="text" id="buyer_name"/>
+            <label htmlFor="buyer_tel">tel</label>
+            <input type="text" id="buyer_addr"/>
+
+            <div>
+                <input type="text" id="postcode" placeholder="우편번호"/>
+                <input type="button" value="우편번호 찾기" id="postcode-btn" onClick={onClick}/><br/>
+                <div className="daum">
+                    <Postcode/>
+                </div>
+            </div>
+            
+            <input type="text" id="address" placeholder="주소"/><br/>
+            <input type="text" id="detailAddress" placeholder="상세주소"/>
+        </form>
     </div>
     )
 }
--- shopping/src/components/MyInfo/index.jsx
+++
@@ -1,3 +0,0 @@
-export default function MyInfo() {
-  return <div className="myInfo"></div>;
-}
