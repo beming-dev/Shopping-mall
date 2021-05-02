8edcd0c7c7f596b27216adefe0d75bcf83f8d1e7
progmins <mingfordev@gmail.com>
2021-05-02 11:27:43 +0900

    update product register page

--- front/src/domain/ProductRegistration/index.jsx
+++ front/src/domain/ProductRegistration/index.jsx
@@ -1,4 +1,5 @@
 import react from "react";
+import "./style.css";
 
 import Header from "../../components/Header/index";
 import Footer from "../../components/Footer/index";
@@ -9,7 +10,16 @@ export default function ProductRegistration() {
     <div>
       <Header />
       <Nav />
-      <div></div>;
+      <div className="product-registration">
+        <form action="" method="post" className="register-form">
+          <label htmlFor="business-number">business-number</label>
+          <input id="business-number" type="text" />
+          <label htmlFor="phone">phone number</label>
+          <input id="phone" type="text" />
+          <input type="submit" className="submit" />
+        </form>
+      </div>
+      ;
       <Footer />
     </div>
   );
